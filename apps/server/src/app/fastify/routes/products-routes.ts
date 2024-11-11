8b2cfd8c6ab39449d8a01e6086f6e1b19663f852
kaufon <kauanfvale@gmail.com>
2024-11-11 15:58:43 -0300

    🚧WIP: products routes

--- apps/server/src/app/fastify/routes/products-routes.ts
+++ apps/server/src/app/fastify/routes/products-routes.ts
@@ -1,9 +1,16 @@
 import type { FastifyInstance } from "fastify";
 import { FastifyHttp } from "../fastify-http";
+import { ListProductsController, RegisterProductController } from "apps/server/src/api/controllers/products";
 
 export const ProductsRoutes = async(app: FastifyInstance) => {
+  const listProductsController = new ListProductsController()
+  const registerProductController = new RegisterProductController()
   app.get('/',async (request,response) => {
     const http = new FastifyHttp(request,response)
-    return 
+    return listProductsController.handle(http)
+  })
+  app.post('/',async (request,response) => {
+    const http = new FastifyHttp(request,response)
+    return registerProductController.handle(http)
   })
 }
