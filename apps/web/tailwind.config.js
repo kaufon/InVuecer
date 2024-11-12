332e93b18fcfdaaa0f1cea58dc8819b96fcfc95c
kaufon <kauanfvale@gmail.com>
2024-11-12 18:04:22 -0300

    🐛fix: fix tailwind content path

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-	content: [],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"], // This defines the files Tailwind will scan for classes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
+
